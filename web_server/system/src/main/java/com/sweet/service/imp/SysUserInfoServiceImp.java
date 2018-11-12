755db86729aeb74ab64f43430e1a520611b3cda0
Smilehzy <hu_ziyang@zfwksoft.com>
2018-11-12 23:08:35 +0800

    事务

--- web_server/system/src/main/java/com/sweet/service/imp/SysUserInfoServiceImp.java
+++ web_server/system/src/main/java/com/sweet/service/imp/SysUserInfoServiceImp.java
@@ -3,6 +3,7 @@ package com.sweet.service.imp;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sweet.mapper.SysUserInfoMapper;
 import com.sweet.service.SysUserInfoService;
@@ -14,6 +15,7 @@ public class SysUserInfoServiceImp implements SysUserInfoService{
 	@Resource
 	private SysUserInfoMapper sysUserInfoMapper;
 	
+	@Transactional
 	public int addUser(String loginid, String password, String phone, Integer sex) {
 		return sysUserInfoMapper.addUser(loginid, password, phone, sex);
 	}
