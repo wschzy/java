b3791e981e0402f4fa63e5e42e7fdb317516b38b
wsc <1027562821@qq.com>
2018-11-12 22:46:44 +0800

    login

---
+++ web_front_end/system/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls:['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+    constructor(){}
+    ngOnInit(){}
+}
