0e747cf1758152acdaf2a6169e66947834c1ea00
Smilehzy <hu_ziyang@zfwksoft.com>
2018-12-26 01:44:22 +0800

    ...

--- web_front_end/system/src/app/page/header/header.component.ts
+++ web_front_end/system/src/app/page/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { InterfaceService } from 'src/app/interface/interface.component';
-import { element } from '@angular/core/src/render3';
-import * as $ from 'jQuery';
 @Component({
     selector: 'storeHeader',
     templateUrl: 'header.component.html',
@@ -10,18 +8,15 @@ import * as $ from 'jQuery';
 })
 
 export class HeaderComponent implements OnInit {
-    list;
     constructor(private myRouter:Router,private service:InterfaceService) {
     }
     ngOnInit() { 
-        this.init();
     }
 
     
-    init(){
+    ai(){
         this.service.interface("SysUserInfo/getMenu.do", null,  
             function(data:any){
-                this.list.push(data);
             }
         );
     }
