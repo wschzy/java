07198005ae53f8e770b002ca11934324ad44476d
Smilehzy <hu_ziyang@zfwksoft.com>
2019-01-15 11:44:41 +0800

    dic

--- web_server/system/src/main/java/com/sweet/bean/UserDictionary.java
+++ web_server/system/src/main/java/com/sweet/bean/UserDictionary.java
@@ -3,6 +3,7 @@ package com.sweet.bean;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -13,10 +14,12 @@ public class UserDictionary implements Serializable {
 	private Integer id;
 	@Pattern(regexp="^\\S*$",message="名称格式错误")
 	@NotNull(message = "名称不能为空")
-	@Size(max = 39, message = "用户名最大39位")
+	@Size(min=1,max = 39, message = "用户名最小1位，最大39位")
 	private String name;
 	private Integer supnumber;
+	@Pattern(regexp="^\\S*$",message="类别格式错误")
 	@NotNull(message = "类别不能为空")
+	@Size(min=1,max = 39, message = "类别最小1位，最大39位")
 	private String dicclass;
 	private String note;
 	private Integer levels;
