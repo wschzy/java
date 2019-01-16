9ec212e53eb3cb4686a5ca7db70810a73450474f
Smilewsc <1027562821@qq.com>
2019-01-16 14:16:15 +0800

    category

--- web_front_end/system/src/app/page/category/category.component.ts
+++ web_front_end/system/src/app/page/category/category.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InterfaceService } from 'src/app/interface/interface.component';
 import {Router} from '@angular/router';
-import throttleByAnimationFrame from 'ng-zorro-antd/core/util/throttleByAnimationFrame';
 @Component({
     selector: 'storeCategory',
     templateUrl: 'category.component.html',
@@ -13,47 +12,37 @@ export class CategoryComponent implements OnInit {
     name:any="";
     dicclass:any="";
     show:any=false;
-    dele:any=false;
-    allChecked = false;
     dataSet= [];
     indeterminate = false;
     constructor(private service:InterfaceService,private router:Router) { }
+
+
     // 弹出添加页面
     add(){
       if(this.show == false){
         this.show=true;
       }
      }
-    //  删除信息
-    deleteRow(i:string){
-      var that=this;
-      const dataSet = this.dataSet.filter(d => d.id !== i);
-      this.dataSet = dataSet;
-      this.service.interface("/category/deleteUserDictionary.do",dataSet,
-        function(){
-          that.ngOnInit();
-        })
-    }
+
     ngOnInit(){
       var that=this;
       this.service.interface("/category/getUserDictionaryList.do",null,
-        function(data){
+        function(data){  
             that.dataSet=data;
         })
       }
-    // 表格 分页 和全选
-    refreshStatus(): void {
-      const allChecked = this.dataSet.every(value => value.checked === true);
-      const allUnChecked = this.dataSet.every(value => !value.checked);
-      this.allChecked = allChecked;
-      this.indeterminate = (!allChecked) && (!allUnChecked);
-    }
-  
-    checkAll(value: boolean): void {
-      this.dataSet.forEach(data => data.checked = value);
-      this.refreshStatus();
-    }
-    // 添加提交
+   
+     //删除信息
+    deleteRow(i: string): void {
+      var that=this;
+      var obj = {id:i};
+      this.service.interface("/category/deleteUserDictionary.do",obj,function(){
+          // const dataSet = that.dataSet.filter(d => d.id !== i);
+          // this.dataSet = dataSet;
+          that.ngOnInit();
+      })
+    } 
+    //添加提交
      submit(){
        var name=this.name;
        var dicclass=this.dicclass;
@@ -70,11 +59,4 @@ export class CategoryComponent implements OnInit {
           })
        }
      }
-    //  删除提交
-    del(){
-      this.service.interface("/category/deleteUserDictionary.do",null,
-        function(){
-          
-        })
     }
-}  
