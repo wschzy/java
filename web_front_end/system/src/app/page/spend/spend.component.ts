f8b324d8a06c5c4da88901c4efb7adecb8cb2d50
Smilewsc <1027562821@qq.com>
2019-01-23 17:53:50 +0800

    select

--- web_front_end/system/src/app/page/spend/spend.component.ts
+++ web_front_end/system/src/app/page/spend/spend.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { InterfaceService } from 'src/app/interface/interface.component';
 import {Router} from '@angular/router';
 import throttleByAnimationFrame from 'ng-zorro-antd/core/util/throttleByAnimationFrame';
+import { discoverDirectives } from '@angular/core/src/render3/context_discovery';
 @Component({
     selector: 'storeSpend',
     templateUrl: 'spend.component.html',
@@ -15,9 +16,9 @@ export class SpendComponent implements OnInit {
     dataSet = [];
     selectedDicclass='伙食';
     selectedName="早餐";
-    selectedId="47";
     dicclass:any=[];
     name:any=[];
+    id:any=[];
     obj:object={};
     constructor(private service:InterfaceService,private router:Router){}
     addRow(){
@@ -29,59 +30,44 @@ export class SpendComponent implements OnInit {
             if(this.obj[d.dicclass] == undefined){
                 this.obj[d.dicclass] = [];
             }
-            var obj2={};
+            var obj2 = {};
             obj2[d.name]=d.id;
-            this.obj[d.dicclass].push(obj2);
-            console.log(obj2);
-
+            this.obj[d.dicclass].push(obj2);  
         }
-        var v=this.selectedDicclass;
-        var a=this.obj[v];
-        for ( var name in a){
-            var o=a[name];
-            for(var n in o){
+        // 第一级 是数组
+        for (var dic in this.obj){
+            this.dicclass.push(dic);
+        }
+    }
+     //  第二级
+    change(){
+        this.name = [];
+        var a=this.selectedDicclass;
+        for(var o of this.obj[a]){
+            for(var n in o ){
                 this.name.push(n);
             }
         }
+            
     }
-    provinceChange(value: string): void {
-        this.selectedName = this.obj[ value ][ 0 ];
-      }
-    // provinceChange(value: string): void {
-    //     this.selectedCity = this.cityData[ value ][ 0 ];
-    //   }
+
+    // 第三级
+    changeId(){
+        this.id=[];
+    }
     cancel(){
         if(this.add == true){
             this.add=false;
         }
-    }   
-    startEdit(id: string): void {
-        this.editCache[ id ].edit = true;
-    }
+    } 
     
-    finishEdit(id: string): void {
-        this.editCache[ id ].edit = false;
-        this.dataSet.find(item => item.id === id).name = this.editCache[ id ].name;
-    }
-    
-    updateEditCache(): void {
-    this.dataSet.forEach(item => {
-        if (!this.editCache[ item.id ]) {
-        this.editCache[ item.id ] = {
-            edit: false,
-            note: item.note
-               };
-            }
-        });
-    }
     ngOnInit(): void {
         var that=this;
         this.service.interface("/pay/getUserPayList.do",null,
             function(data:any){
                 that.dataSet=data;
-                console.log(that.dataSet);
             });
-            this.updateEditCache();
+            
     }
     deleteRow(i: string): void {
         var that=this;
@@ -92,4 +78,7 @@ export class SpendComponent implements OnInit {
         })
         
     }
+    provinceChange(value: string): void {
+        this.selectedName = this.obj[ value ][ 0 ];
+      }
 }
