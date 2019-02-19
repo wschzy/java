6d5adec24e8b7a2f2177dfb27cdfee4022924a71
Smilehzy <hu_ziyang@zfwksoft.com>
2019-02-19 17:02:04 +0800

    pay

--- web_front_end/system/src/app/page/index/index.component.ts
+++ web_front_end/system/src/app/page/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InterfaceService } from 'src/app/interface/interface.component';
 @Component({
     selector: 'storeIndex',
     templateUrl: 'index.component.html',
@@ -6,24 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class IndexComponent implements OnInit {
-    options:any;
-    constructor() { }
-
-    ngOnInit() { 
-        this.options = {
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [{
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line'
-            }]
-        };
+    optionsByWeek:any;
+    constructor(private service:InterfaceService) { }
+   
+    ngOnInit() {
+        //查询本周每天消费
+        var that = this;
+        this.service.interface("pay/getMoneyListByWeek.do",null,
+          function(data){
+              var dt = {'Mon':0,'Tue':0,'Wed':0,'Thu':0,'Fri':0,'Sat':0,'Sun':0};
+              for(var i of data){
+                var day = i.obj;
+                var money = i.money;
+                dt[day] = money;
+              }
+              that.optionsByWeek = {
+                legend: {
+                    data:['本周每天消费情况']
+                },
+                xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    name:'本周每天消费情况',
+                    label: {
+                        normal: {
+                            show: true,
+                            textStyle: {
+                              color: 'black'
+                            }
+                        }
+                   },
+                    data: [dt.Mon,dt.Tue,dt.Wed,dt.Thu,dt.Fri,dt.Sat,dt.Sun],
+                    type: 'line'
+                }]
+              };
+        })
     }
-    
-    
 }
--- web_server/system/src/main/java/com/sweet/hzy/mapper/provider/UserPayProvider.java
+++ web_server/system/src/main/java/com/sweet/hzy/mapper/provider/UserPayProvider.java
@@ -18,23 +18,22 @@ public class UserPayProvider {
 	
 	public String getMoneyListByWeek(Integer userid,Date time) {
 		String timeWhere = getTime(time);
-		return "SELECT sum(MONEY)money,date_format(TIME,'%Y-%m-%d')time FROM user_pay WHERE (userid = #{userid} or userid in "
-				+ "(select userid from user_home_rel where homeid in (select homeid from user_home_rel where userid = #{userid}))) "
+		return "SELECT sum(MONEY)money,date_format(TIME,'%Y-%m-%d')time,DATE_FORMAT(time, '%a') obj FROM user_pay WHERE (userid = "+userid+" or userid in "
+				+ "(select userid from user_home_rel where homeid in (select homeid from user_home_rel where userid = "+userid+"))) "
 				+ "and YEARWEEK(date_format(time, '%Y-%m-%d') - INTERVAL 1 DAY) = YEARWEEK("+timeWhere+" - INTERVAL 1 DAY) group by date_format(TIME,'%Y-%m-%d')";
 	}
 	
-	
 	public String getMoneyListByMonth(Integer userid,Date time) {
 		String timeWhere = getTime(time);
-		return "SELECT money,date_format(TIME,'%Y-%m-%d')time FROM user_pay WHERE (userid = #{userid} or  userid in" + 
-		"(select userid from user_home_rel where homeid in (select homeid from user_home_rel where userid = #{userid)))" + 
+		return "SELECT money,date_format(TIME,'%Y-%m-%d')time FROM user_pay WHERE (userid = "+userid+" or  userid in" + 
+		"(select userid from user_home_rel where homeid in (select homeid from user_home_rel where userid = "+userid+"))" + 
 		"and date_format(time, '%Y-%m')  = date_format("+timeWhere+", '%Y-%m') order by time";
 	}
 	
 	public String getMoneyListByYear(Integer userid,Date time) {
 		String timeWhere = getTime(time);
-		return "SELECT sum(MONEY)money,date_format(TIME,'%m')time FROM user_pay WHERE (userid = #{userid} or userid in "
-		+ "(select userid from user_home_rel where homeid in (select homeid from user_home_rel where userid = #{userid}))) "
+		return "SELECT sum(MONEY)money,date_format(TIME,'%m')time FROM user_pay WHERE (userid = "+userid+" or userid in "
+		+ "(select userid from user_home_rel where homeid in (select homeid from user_home_rel where userid = "+userid+"))) "
 		+ "and date_format(time, '%Y')  = date_format("+timeWhere+", '%Y') group by date_format(TIME,'%Y-%m')";
 	}
 }
