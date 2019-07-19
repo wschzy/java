27b4e976bcc44e8d048ec5fba5b2918e68a10533
Smilehzy <hu_ziyang@zfwksoft.com>
2019-07-19 15:06:01 +0800

    SpringBoot+mybatis+redis

--- web_server/system/src/main/java/com/sweet/Action.java
+++ web_server/system/src/main/java/com/sweet/Action.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import com.sweet.config.DBConfig1;
 import com.sweet.config.DBConfig2;
 @SpringBootApplication// same as @Configuration @EnableAutoConfiguration @ComponentScan
-@EnableAsync//开启异步
+//@EnableAsync//开启异步
 @EnableConfigurationProperties(value = { DBConfig1.class, DBConfig2.class })//多数据源
 @ServletComponentScan//在 SpringBootApplication 上使用@ServletComponentScan 注解后，Servlet、Filter、Listener 可以直接通过 @WebServlet、@WebFilter、@WebListener 注解自动注册，无需其他代码
 public class Action {
