47e4565b00b4c14300cd448711c1da2902d7ab5f
Smilehzy <hu_ziyang@zfwksoft.com>
2020-03-03 20:55:11 +0800

    rediscluster集群

--- web_server/system/src/main/java/com/sweet/redis/RedisPool.java
+++ web_server/system/src/main/java/com/sweet/redis/RedisPool.java
@@ -1,31 +1,58 @@
 package com.sweet.redis;
 
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisPool {
-    private static JedisPool pool;
+    private static JedisCluster pool;
+
+    @Value("${spring.redis.jedis.cluster.nodes}")
+    private String clusterNodes;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private String maxActive;
+    @Value("${spring.redis.jedis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.jedis.commandTimeout}")
+    private int commandTimeout;
+    @Value("${spring.redis.jedis.password}")
+    private String password;
+    @Value("${spring.redis.jedis.pool.testOnBorrow}")
+    private String testOnBorrow;
+    @Value("${spring.redis.jedis.pool.testOnReturn}")
+    private String testOnReturn;
 
     private RedisPool(){
-        ResourceBundle bundle =ResourceBundle.getBundle("application");
-        //bundle类似一个map
-        if(bundle==null){
-            throw new IllegalArgumentException("[redis.properties] is not find ");
-           }
+        //ResourceBundle bundle =ResourceBundle.getBundle("application");if(bundle==null){ throw new IllegalArgumentException("[redis.properties] is not find "); }
         JedisPoolConfig config = new JedisPoolConfig();
-        config.setMaxTotal(Integer.valueOf(bundle.getString("spring.redis.jedis.pool.max-active")));
-        config.setMaxIdle(Integer.valueOf(bundle.getString("spring.redis.jedis.pool.max-idle")));
-        config.setMaxWaitMillis(Long.valueOf(bundle.getString("spring.redis.jedis.pool.max-wait")));
-        config.setTestOnBorrow(Boolean.valueOf(bundle.getString("spring.redis.jedis.pool.testOnBorrow")));
-        config.setTestOnReturn(Boolean.valueOf(bundle.getString("spring.redis.jedis.pool.testOnReturn")));
-
+        config.setMaxIdle(Integer.valueOf(maxIdle));
+        config.setMaxWaitMillis(Long.valueOf(maxWaitMillis));
+        config.setMaxTotal(Integer.valueOf(maxActive));
+        config.setTestOnBorrow(Boolean.valueOf(testOnBorrow));
+        config.setTestOnReturn(Boolean.valueOf(testOnReturn));
+        String[] cNodes = clusterNodes.split(",");
+        Set<HostAndPort> nodes = new HashSet<>();
+        //分割出集群节点
+        for (String node : cNodes) {
+            String[] nd = node.split(":");
+            nodes.add(new HostAndPort(nd[0], Integer.parseInt(nd[1])));
+        }
         //创建连接池
-        pool =new JedisPool(config,bundle.getString("spring.redis.host"),Integer.valueOf(bundle.getString("spring.redis.port")),Integer.valueOf(bundle.getString("spring.redis.timeout")));
+        //pool =new JedisPool(config,bundle.getString("spring.redis.host"),Integer.valueOf(bundle.getString("spring.redis.port")),Integer.valueOf(bundle.getString("spring.redis.timeout")));
+        pool = new JedisCluster(nodes,commandTimeout,timeout,5,password, config);
     }
 
-    public  static JedisPool getPool() {
+    public  static JedisCluster getPool() {
         if(pool==null){
             synchronized (RedisPool.class) {
                 if(pool==null){
